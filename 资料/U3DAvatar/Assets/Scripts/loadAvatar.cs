aac701959561a06eb9ae4a6f87864791e15e0f1a
kangwen <1397796710@qq.com>
2022-09-20 16:26:41 +0800

    Create Project.

---
+++ 资料/U3DAvatar/Assets/Scripts/loadAvatar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadAvatar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (AvatarSys._instance.nowCount == 0)
+        {
+            AvatarSys._instance.GirlAvatar();
+        }
+        else {
+            AvatarSys._instance.BoyAvatar();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

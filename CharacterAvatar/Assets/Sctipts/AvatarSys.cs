bd5929f2e559ddc8f325fbb9478059495c0588e6
kangwen <1397796710@qq.com>
2022-09-22 20:29:28 +0800

    增加UserText开关控制。

--- CharacterAvatar/Assets/Sctipts/AvatarSys.cs
+++ CharacterAvatar/Assets/Sctipts/AvatarSys.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class AvatarSys : MonoBehaviour
 {
+    public bool useTxt = false;
+    
     private Transform _girlSourceTrans;
     private GameObject _girlTarget;     // 骨架物体，换装的人
     private Transform[] _girlHips;      //girl的骨骼信息
@@ -20,20 +22,101 @@ public class AvatarSys : MonoBehaviour
     private Dictionary<string, Dictionary<string, SkinnedMeshRenderer>> _girlData = new Dictionary<string, Dictionary<string, SkinnedMeshRenderer>>();
     
     //初始化信息，部位的名字，部位对应的skm
-    private string[,] _girlStr = { {"eyes","1"},{"hair","1"},{"top","1"},{"pants","1"},{"shoes","1"},{"face","1"}};
-
-    #region Test
     public string[] girlSkins = {"eyes-2", "hair-2", "top-2", "pants-2", "shoes-2", "face-2"};
+
+    private void Start()
+    {
+        InstantiateTarget();
+        if (!useTxt)
+        {
+            InstantiateSource();
+            SaveData();    
+        }
+        RefreshAvatar();
+    }
+
+    private void InstantiateTarget()
+    {
+        _girlTarget = Instantiate(Resources.Load("FemaleTarget")) as GameObject;
+        if (!_girlTarget) return;
+        _girlHips = _girlTarget.GetComponentsInChildren<Transform>();
+    }
+
+    private void InstantiateSource()
+    {
+        GameObject go = Instantiate(Resources.Load("FemaleModel")) as GameObject;
+        if (!go) return;
+        _girlSourceTrans = go.transform;
+        go.SetActive(false);
+    }
+
+    void SaveData()
+    {
+        if (_girlSourceTrans == null) return;
+
+        //找到sourceTran中所有的SkinnedMeshRenderer，进行存储
+        SkinnedMeshRenderer[] parts = _girlSourceTrans.GetComponentsInChildren<SkinnedMeshRenderer>();
+        foreach (var part in parts)
+        {
+            string[] names = part.name.Split('-');
+            var partName = names[0];
+            if (!_girlData.ContainsKey(partName))
+            {
+                // 生成对应的部位，且只生成一个
+                GameObject partGo = new GameObject(partName);
+                partGo.transform.parent = _girlTarget.transform;
+                // 把骨骼target身上的skm信息存储
+                _girlSmr.Add(partName, partGo.AddComponent<SkinnedMeshRenderer>());
+                _girlData.Add(partName, new Dictionary<string, SkinnedMeshRenderer>());
+            }
+            _girlData[partName].Add(names[1], part);    // 存储所有的skm信息到数据里面
+        }
+    }
 
     void RefreshAvatar()
     {
         foreach (var str in girlSkins)
         {
-            // var array = str.Split('-');
-            // ChangeMesh(array[0], array[1]);
-            ChangeMesh2(str);
+            if (useTxt)
+            {
+                ChangeMesh2(str);
+            }
+            else
+            {
+                var array = str.Split('-');
+                ChangeMesh(array[0], array[1]);    
+            }
         }
     }
+    
+    /// <summary>
+    /// 换装实现
+    /// </summary>
+    /// <param name="part">部位</param>
+    /// <param name="num">编号</param>
+    void ChangeMesh(string part, string num)
+    {
+        SkinnedMeshRenderer skm = _girlData[part][num];
+        List<Transform> bones = new List<Transform>();
+        foreach (var trans in skm.bones)
+        {
+            foreach (var bone in _girlHips)
+            {
+                if (bone.name == trans.name)
+                {
+                    bones.Add(bone);
+                    break;
+                }
+            }
+        }
+        //换装实现
+        var curSkm = _girlSmr[part];
+        curSkm.bones = bones.ToArray();
+        curSkm.materials = skm.materials;
+        curSkm.sharedMesh = skm.sharedMesh;
+    }
+    
+    #region Test
     
     private void Update()
     {
@@ -85,90 +168,4 @@ public class AvatarSys : MonoBehaviour
         curSkm.sharedMesh = mesh;
     }
     #endregion
-     
-    private void Start()
-    {
-        // InstantiateSource();
-        InstantiateTarget();
-        
-        // SaveData();
-        // InitAvatar();
-    }
-
-    private void InstantiateSource()
-    {
-        GameObject go = Instantiate(Resources.Load("FemaleModel")) as GameObject;
-        if (!go) return;
-        _girlSourceTrans = go.transform;
-        go.SetActive(false);
-    }
-
-    private void InstantiateTarget()
-    {
-        _girlTarget = Instantiate(Resources.Load("FemaleTarget")) as GameObject;
-        if (!_girlTarget) return;
-        _girlHips = _girlTarget.GetComponentsInChildren<Transform>();
-    }
-
-    void SaveData()
-    {
-        if (_girlSourceTrans == null) return;
-
-        //找到sourceTran中所有的SkinnedMeshRenderer，进行存储
-        SkinnedMeshRenderer[] parts = _girlSourceTrans.GetComponentsInChildren<SkinnedMeshRenderer>();
-        foreach (var part in parts)
-        {
-            string[] names = part.name.Split('-');
-            var partName = names[0];
-            if (!_girlData.ContainsKey(partName))
-            {
-                // 生成对应的部位，且只生成一个
-                GameObject partGo = new GameObject(partName);
-                partGo.transform.parent = _girlTarget.transform;
-                // 把骨骼target身上的skm信息存储
-                _girlSmr.Add(partName, partGo.AddComponent<SkinnedMeshRenderer>());
-                _girlData.Add(partName, new Dictionary<string, SkinnedMeshRenderer>());
-            }
-            _girlData[partName].Add(names[1], part);    // 存储所有的skm信息到数据里面
-        }
-    }
-
-    /// <summary>
-    /// 换装实现
-    /// </summary>
-    /// <param name="part">部位</param>
-    /// <param name="num">编号</param>
-    void ChangeMesh(string part, string num)
-    {
-        SkinnedMeshRenderer skm = _girlData[part][num];
-        List<Transform> bones = new List<Transform>();
-        foreach (var trans in skm.bones)
-        {
-            foreach (var bone in _girlHips)
-            {
-                if (bone.name == trans.name)
-                {
-                    bones.Add(bone);
-                    break;
-                }
-            }
-        }
-        //换装实现
-        var curSkm = _girlSmr[part];
-        curSkm.bones = bones.ToArray();
-        curSkm.materials = skm.materials;
-        curSkm.sharedMesh = skm.sharedMesh;
-    }
-
-    /// <summary>
-    /// 初始化骨架，让它有mesh 材质 骨骼信息
-    /// </summary>
-    void InitAvatar()
-    {
-        int length = _girlStr.GetLength(0);
-        for (int i = 0; i < length; i++)
-        {
-            ChangeMesh(_girlStr[i, 0], _girlStr[i, 1]);
-        }
-    }
 }
--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # CharacterAvatar
 CharacterAvatar-Unity换装
 
+### 工具用法
+GenerateSkinDataEditor 会生成mesh和txt。
+
+其中txt存储格式如下：
+
+```
+第1行存储material数量，设为n
+第2行开始存储material的名字
+第n+2行存储mesh的名字
+第n+3行存储rootBon
+第n+4行开始都是bones
+```
+示例：
+```
+1（材质球数量）
+female_pants-1_green（材质球名称）
+pants-1（mesh名字）
+Female_Hips（RootBone, 这行之后为BoneList）
+Female_Hips/Female_RightUpLeg
+Female_Hips
+Female_Hips/Female_RightUpLeg/Female_RightLeg
+Female_Hips/Female_RightUpLeg/Female_RightLeg/Female_RightFoot
+Female_Hips/Female_LeftUpLeg
+Female_Hips/Female_LeftUpLeg/Female_LeftLeg
+Female_Hips/Female_LeftUpLeg/Female_LeftLeg/Female_LeftFoot
+Female_Hips/Female_Spine
+```
+
+### 链接
 https://github.com/getker/CharacterAvatar
 
 https://www.sikiedu.com/course/130
